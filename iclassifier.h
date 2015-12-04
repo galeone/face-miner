1adf2ff4b64b68f186d97c2165b3c9cae15dea1d
Paolo Galeone <nessuno@nerdz.eu>
2015-12-04 09:52:51 +0100

    Defining ICLassifier interface

--- iclassifier.h
+++ iclassifier.h
@@ -2,12 +2,22 @@
 #define ICLASSIFIER_H
 
 #include <opencv2/core.hpp>
+#include <string>
+
+class ILabel {
+public:
+    virtual ~ILabel(){}
+    std::string get();
+};
 
 class IClassifier
 {
 public:
     virtual ~IClassifier(){}
-    virtual bool isA(cv::Mat&) = 0;
+    virtual float classify(const cv::Mat &) = 0;
+    virtual void train(ILabel &, const cv::Mat &) = 0;
+    virtual float getThreshold() = 0;
+
 };
 
 #endif // ICLASSIFIER_H
